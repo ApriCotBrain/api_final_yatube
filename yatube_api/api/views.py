d43ce5a3e1eb26bed5f350d31f6095ed7528737a
Ольга Мелихова <melihova.o@yandex.ru>
2022-11-24 15:57:04 +0300

    second iteration pep8

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
 from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (
